45dedb0315482dfbcec5eb19d1a5bec45aa7c840
unknown <ruel_y2k@yahoo.com>
2019-08-21 12:42:51 +0800

    post exercise

--- workshop01/main.js
+++ workshop01/main.js
@@ -81,6 +81,8 @@ app.get('/information', (req, resp) => {
     //Weather for city is in cityName variable
     //API key is in keys.weather
     const params = {
+        q: cityName,
+        appid: keys.weather
     }
 
     getWeather(params)
@@ -93,6 +95,9 @@ app.get('/information', (req, resp) => {
             //The 2 character country code is found in countryCode variable
             //API key is in keys.news
             const params = {
+                country: countryCode,
+                category: 'technology',
+                apikey: keys.news
             }
             return (Promise.all([ result, getNews(params) ]));
         })
